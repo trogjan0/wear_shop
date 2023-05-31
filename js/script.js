abdaf08d640449ccd1c3a6c5400f3aac893cd566
trogjan <trogjannn@yandex.ru>
2023-05-31 03:02:54 +0300

    create

---
+++ js/script.js
@@ -0,0 +1,75 @@
+
+document.querySelector('.button__side-bar').addEventListener('click', () => {
+    document.querySelector('.side-bar').classList.add('active');
+    document.querySelector('.close__side-bar').classList.add('active');
+  })
+
+document.querySelector('.close__side-bar').addEventListener('click', () => {
+  document.querySelector('.side-bar').classList.remove('active');
+  document.querySelector('.close__side-bar').classList.remove('.close__side-bar.active');
+})
+
+document.querySelector('.overlay').addEventListener('click', () => {
+  document.querySelector('.side-bar').classList.remove('active');
+  document.querySelector('.overlay').classList.remove('.overlay.active');
+})
+
+document.querySelector('.close__ad').addEventListener('click', () => {
+  document.querySelector('.ad').classList.add('disactive');
+})
+
+
+let slider = document.querySelector('.slider');
+let slider__items = slider.querySelectorAll('.slider__item');
+let buttons = slider.querySelectorAll('.btn__slider');
+let button__prev = buttons[0];
+let button__next = buttons[1];
+let maxItems = slider__items.length;
+
+let num = 0;
+
+function transformNum(znak) {
+  let backElem, frontElem;
+  if (znak === 'minus') {
+    backElem = slider__items[num + 1];
+    frontElem = slider__items[num];
+    backElem.style.transform = `translateZ(0px) translate(${-1 * num * 100}%, 0) scale(0)`;
+    frontElem.style.transform = `translateZ(0px) translate(${-1 * num * 100}%, 0) scale(1)`;
+  } else if (znak === 'plus') {
+    backElem = slider__items[num - 1];
+    frontElem = slider__items[num];
+    backElem.style.transform = `translateZ(0px) translate(${-1 * num * 100}%, 0) scale(0)`;
+    frontElem.style.transform = `translateZ(0px) translate(${-1 * num * 100}%, 0) scale(1)`;
+  }
+}
+
+function addActive(znak) {
+  let elem;
+  if (znak === 'minus') {
+    if (num - 1 === -1) {
+      return;
+    }
+    num--;
+    transformNum(znak);
+    elem = slider__items[num + 1];
+    elem.classList.remove('active');
+  } else if (znak === 'plus') {
+    if (num + 1 === maxItems) {
+      return;
+    }
+    num++;
+    transformNum(znak);
+    elem = slider__items[num - 1];
+    elem.classList.remove('active');
+  }
+  elem = slider__items[num];
+  elem.classList.add('active');
+}
+
+button__prev.addEventListener('click', () => {
+  addActive('minus');
+});
+
+button__next.addEventListener('click', () => {
+  addActive('plus');
+});
